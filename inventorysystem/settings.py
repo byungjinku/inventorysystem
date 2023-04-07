8786c707c37e535e7552eb0c370ae6f067f5b5b3
byungjinku <66653030+byungjinku@users.noreply.github.com>
2023-04-07 15:09:51 +0900

    Update settings.py

--- inventorysystem/settings.py
+++ inventorysystem/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8j89*3kcnct(8vtckj@voidg1-t)@10ezxpqfrcyb%6_v^^xo2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
